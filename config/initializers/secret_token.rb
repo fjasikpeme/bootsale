e182aa46363f8bc3e1c8bbb776b3d84528eb2f23
Frederick Jas-Ikpeme <fjas.ikpeme@gmail.com>
2012-07-09 13:25:42 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootsale::Application.config.secret_token = '33e59446fc3eb9724dbf9b1ad77d63c9aa2e4dd960e323c03524e23f76c3d3cb4392791adee8cc26f821406cb6aa9d154d3cf72f87bec2e40e37b6ef429d8d4b'
